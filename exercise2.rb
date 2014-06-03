06113c31c1827bf6c96d69076edc7050237fad22
Matthew <wong.mat@gmail.com>
2014-06-03 11:47:38 -0400

    ex2

---
+++ exercise2.rb
@@ -0,0 +1,9 @@
+puts "Tip for a 55 dollar meal: " + (55 * 0.15).to_s
+puts "The string adding to " + (5).to_s
+puts "45628 * 7839 = " + (45628 * 7839).to_s
+print "(true && false) || (false && true) || !(false && false) is "
+if (true && false) || (false && true) || !(false && false) == true
+	puts true
+else
+	puts false
+end
